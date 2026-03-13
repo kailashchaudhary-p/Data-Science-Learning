79ec2719d7e042bab33f51909d65111e7d042347
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-03-12 20:20:39 -0800

    update

---
+++ Numpy/1.py
@@ -0,0 +1,4 @@
+#first program in numpy
+import numpy as np
+numpy_array=np.array([45,56,12,4,45,4,54,44,84,4,45,4454,878,45,46,3,4,46,87,87,54,5,484])
+print(numpy_array)
---
+++ Numpy/2.py
@@ -0,0 +1,15 @@
+#The types of array
+#one dimention array
+import numpy as np
+one_dimention=np.array([445,15,465,54654,6553])
+print(one_dimention)
+#two dimention array 
+two_dimention=np.array([[45,47,32],
+                        [85,45,74],
+                        [96,32,52]])
+print(two_dimention)
+#multi dimention
+#matrix
+multi_dimention=([[8,6,2],
+                  [4,1,9]])
+print(multi_dimention)
