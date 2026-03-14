fc055aa06d6c645fac5918a087ce3aff150052d9
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-03-13 21:54:42 -0800

    update

---
+++ Numpy/chapter 2/1.py
@@ -0,0 +1,7 @@
+#IN this code we will leaern the arrays shape , size or type 
+import numpy as np
+array=np.array([[1,2,3],[4,5,6]])
+print(array.shape)#this is check the shape of an array 
+
+array_b=np.array([[1,2,3],[4,5,6]])
+print(array_b.size)#this is check the array size 
