47c9cc907c3b7008cfae5a1444770211ff46b20e
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-03-14 20:55:13 -0800

    update

--- Numpy/chapter 2/1.py
+++ Numpy/chapter 2/1.py
@@ -4,4 +4,15 @@ array=np.array([[1,2,3],[4,5,6]])
 print(array.shape)#this is check the shape of an array 
 
 array_b=np.array([[1,2,3],[4,5,6]])
-print(array_b.size)#this is check the array size 
+print(array_b.size)#this is check the array size  
+
+
+#array n dim it meanes number dimention
+array_c=np.array([5,7,9,6,4])
+print(array_c.ndim)
+
+#d type it meanes the data types of element
+array_d=np.array([[7,8,9,4],[1,2,3,4,5]])
+print(array_d.dtype)
+
+#convert data type
