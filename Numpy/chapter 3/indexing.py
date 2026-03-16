2c69867cb4e35982a1cbc1a468cae712b5fc5d05
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-03-16 13:33:40 -0800

    update

---
+++ Numpy/chapter 3/indexing.py
@@ -0,0 +1,11 @@
+#In numpy the array have indexing number which start from zero 
+'''
+for 1d array array[indexing]
+for 2d array array[row,colum]
+'''
+import numpy as np
+array=np.array([1,2,3,4,5,6,7,8,9])
+print(array[0])#first element
+print(array[1])#second 
+print(array[2])#third
+print(array[3])#forth
