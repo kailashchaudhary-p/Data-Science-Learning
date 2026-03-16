5410562b1090ad26e7d3a6cf9a96aa8e5d60d34c
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-03-15 22:00:09 -0800

    update

--- Numpy/chapter 2/3.py
+++ Numpy/chapter 2/3.py
@@ -1,3 +1,10 @@
 #aggregation function
 import numpy as np
 array=np.array([4,5,6,1,2,4,7])
+print(np.sum(array))
+print(np.mean(array))
+print(np.min(array))
+print(np.max(array))
+print(np.std(array))
+print(np.var(array))
+
