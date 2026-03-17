b39e4018f0f872c50067b8263cc255b36d23c31f
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-03-16 19:51:11 -0800

    update

--- Numpy/chapter 3/indexing.py
+++ Numpy/chapter 3/indexing.py
@@ -6,6 +6,7 @@ for 2d array array[row,colum]
 import numpy as np
 array=np.array([1,2,3,4,5,6,7,8,9])
 print(array[0])#first element
-print(array[1])#second 
-print(array[2])#third
-print(array[3])#forth
+print(array[1])#second element
+print(array[2])#third element
+print(array[3])#forth element
+print(array[4])
