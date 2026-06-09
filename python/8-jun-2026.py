8bc0c6d7f02d0dc8dc3ceab6653653c7ca6d5d3e
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-06-08 23:03:01 -0800

    update

---
+++ python/8-jun-2026.py
@@ -0,0 +1,16 @@
+# Dynamic Variables
+name=input("Enter your name: ") #the input function always return a string
+age=int(input("Enter your age: "))#the int function converts the string returned by input into an integer
+print("My name is",name,"and I am",age,"years old.")
+
+#Type Conversion
+height=float(input("ENter your height in meters:"))#the float function converts the string returned float 
+print(type(height))
+print("my height is",height,"meters.")
+a="24"
+print(type(a))
+print("The value of a is string:",a)
+b=int(a)
+print(type(b))
+print("The value of b is int:",b)
+
