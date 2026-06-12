8045b77be9c5a4617fe95132b765b7372881e7a3
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-06-12 22:42:48 +0530

    update

---
+++ python/12-jun-2026.py
@@ -0,0 +1,10 @@
+#for loop 
+for i in range(0,100,1):
+    if i % 3==0 and i%5==0:
+        print("fizzbuzz")
+    elif i % 3==0:
+            print("fizz")
+    elif i % 5==0:
+                print("buzz")
+    else:
+                    print(i)
